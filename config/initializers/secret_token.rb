eedd3c6bb00cb07be9b927b0fe7bdaf4447ff4ef
Admin <anton@ubuntu.ubuntu-domain>
2011-10-19 16:13:51 +0600

    Первый commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kontakt::Application.config.secret_token = 'aa4c4632dac791eb2aeb48d9e926d2a0670d7b96b0e1001511d26b12a2c79084a9a589d67026cbaa1208d884e183c3aa56966b2fda2d1a3f219ee7f20e7d6b32'
